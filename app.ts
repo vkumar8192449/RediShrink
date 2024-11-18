dd0ede1eae314524c81db1b31a80d2477d2da318
vinay kumar <vkumar8192449@gmail.com>
2024-11-19 02:10:14 +0530

    add shortner component

--- app.ts
+++ app.ts
@@ -1,9 +1,17 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser()); // Parse cookies
+
+app.use(
+  cors({
+    origin: process.env.ORIGIN?.split(","),
+    credentials: true,
+  })
+);
 
 import shrinkRouter from "./routes/shrink.routes";
 import redirectRouter from "./routes/redirect.routes";
--- dist/app.js
+++ dist/app.js
@@ -2,14 +2,20 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 exports.app = app;
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)()); // Parse cookies
+app.use((0, cors_1.default)({
+    origin: (_a = process.env.ORIGIN) === null || _a === void 0 ? void 0 : _a.split(","),
+    credentials: true,
+}));
 const shrink_routes_1 = __importDefault(require("./routes/shrink.routes"));
 const redirect_routes_1 = __importDefault(require("./routes/redirect.routes"));
 const stats_routes_1 = __importDefault(require("./routes/stats.routes"));
